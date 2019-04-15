b56c42da663329098a5af5ff9ab3f2afa64b4d39
Elliot Thomas <elliotnthomas@gmail.com>
2019-04-15 17:17:20 -0500

    added path

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+const path = require('path');
 const Pool = require('pg').Pool;
 
 const pool = new Pool({
