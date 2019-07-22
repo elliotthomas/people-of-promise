c01f977a16ea10bde037fe6af27f25362c71c3d1
Elliot Thomas <elliotnthomas@gmail.com>
2019-07-21 21:59:23 -0500

    added assets

--- client/src/Components/Gallery/Gallery.js
+++ client/src/Components/Gallery/Gallery.js
@@ -1,18 +1,51 @@
 import React, { Component } from 'react';
 import './Gallery.css';
+import ReactMediumImg from 'react-medium-zoom'
 
 class Gallery extends Component {
 
   state = {
+    isZoomed: false
+  }
+  zoomIn = () => {
+    this.setState({ isZoomed: true });
+
+  }
+  
+  zoomOut = () => {
+    this.setState({ isZoomed: false });
   }
 
   render() {
 
-    return (
-        <div className = "map-container">
-            <img className ="map cover-image"></img>
 
-        </div>
+    return (
+        <div className = "gallery-container">
+            <div className ="gallery">
+              <div className ="image-1-container">
+              <ReactMediumImg
+              onClick={() => this.state.isZoomed ? this.zoomOut() : this.zoomIn()}
+              className = 'image-1'
+              src={require('../../images/adam-1.png')}
+        />
+              </div>
+              <div className ="image-2-container">
+              <ReactMediumImg className='image-2'
+        src={require('../../images/adam-2.png')}
+        />
+              </div>
+              <div className ="image-3-container">
+              <ReactMediumImg className='image-3'
+        src={require('../../images/adam-3.png')}
+        />
+              </div>
+              <div className = "image-4-container">
+              <ReactMediumImg className='image-4'
+        src={require('../../images/adam-4.jpg')}
+        />
+              </div>
+            </div>
+          </div>
     );
   }
 }
