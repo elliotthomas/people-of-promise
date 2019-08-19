cf690f4e9570f1700d78edcc81a2c282a3464345
Elliot Thomas <elliotnthomas@gmail.com>
2019-08-19 06:39:05 -0500

    added assets

--- client/src/Components/Tiles/Tiles.js
+++ client/src/Components/Tiles/Tiles.js
@@ -10,7 +10,7 @@ class Tiles extends Component {
         <div className = 'outer-div-tiles'>
         <h1 className= 'tiles-header'>Click on the ‘Stepping Stones’ below to locate this week’s lesson</h1>
         <div className ='tiles cover-image'>
-        <a onClick={this.props.clickLink} data-value = '2019-09-08' href= '#introPerson' className = 'calendar-link day-1'>September 8<br />Adam and Eve</a>
+        <a onClick={this.props.clickLink} data-value = '2019-09-08' href= '#introPerson' className = 'calendar-link day-1' style = {{fontSize: '17pt'}}>September 8<br />Adam and Eve</a>
         <a onClick={this.props.clickLink} data-value = '2019-09-15' href= '#introPerson' className = 'calendar-link day-2'>September 15<br />Noah</a>
         <a onClick={this.props.clickLink} data-value = '2019-09-22' href= '#introPerson' className = 'calendar-link day-3'>September 22<br />Abraham & Sarah</a>
         <a onClick={this.props.clickLink} data-value = '2019-09-29' href= '#introPerson' className = 'calendar-link day-4'>September 29<br />Hagar & Ishmael</a>
@@ -18,7 +18,7 @@ class Tiles extends Component {
         <a onClick={this.props.clickLink} data-value = '2019-10-13' href= '#introPerson' className = 'calendar-link day-6'>October 13<br />Joseph</a>
         <a onClick={this.props.clickLink} data-value = '2019-10-20' href= '#introPerson' className = 'calendar-link day-7'>October 20<br />Moses</a>
         <a onClick={this.props.clickLink} data-value = '2019-10-27' href= '#introPerson' className = 'calendar-link day-8'>October 27<br />Miriam</a>
-        <a onClick={this.props.clickLink} data-value = '2019-11-03' href= '#introPerson' className = 'calendar-link day-9'>November 3<br />Joshua & Caleb</a>
+        <a onClick={this.props.clickLink} data-value = '2019-11-03' href= '#introPerson' className = 'calendar-link day-9' style = {{fontSize: '16pt'}}>November 3<br />Joshua & Caleb</a>
         <a onClick={this.props.clickLink} data-value = '2019-11-10' href= '#introPerson' className = 'calendar-link day-10'>November 10<br />Gideon</a>
         <a onClick={this.props.clickLink} data-value = '2019-11-17' href= '#introPerson' className = 'calendar-link day-11'>November 17<br />Samuel</a>
         <a onClick={this.props.clickLink} data-value = '2019-11-24' href= '#introPerson' className = 'calendar-link day-12'>November 24<br />David</a>
@@ -31,7 +31,7 @@ class Tiles extends Component {
         <a onClick={this.props.clickLink} data-value = '2020-01-12' href= '#introPerson' className = 'calendar-link day-20'>January 12<br />Esther</a>
         <a onClick={this.props.clickLink} data-value = '2020-01-19' href= '#introPerson' className = 'calendar-link day-21'>January 19<br />Job</a>
         <a onClick={this.props.clickLink} data-value = '2020-01-26' href= '#introPerson' className = 'calendar-link day-22'>January 26<br />Nehemiah</a>
-        <a onClick={this.props.clickLink} data-value = '2020-02-02' href= '#introPerson' className = 'calendar-link day-23'>February 2<br />Prodigal Father/Son</a>
+        <a onClick={this.props.clickLink} data-value = '2020-02-02' href= '#introPerson' className = 'calendar-link day-23' style = {{fontSize: '16pt'}}>February 2<br />Prodigal Father/Son</a>
         <a onClick={this.props.clickLink} data-value = '2020-02-09' href= '#introPerson' className = 'calendar-link day-24'>February 9<br />Nicodemus</a>
         <a onClick={this.props.clickLink} data-value = '2020-02-16' href= '#introPerson' className = 'calendar-link day-25'>February 16<br />Lazarus</a>
         <a onClick={this.props.clickLink} data-value = '2020-02-23' href= '#introPerson' className = 'calendar-link day-26'>February 23<br />Mary & Martha</a>
@@ -39,11 +39,11 @@ class Tiles extends Component {
         <a onClick={this.props.clickLink} data-value = '2020-03-08' href= '#introPerson' className = 'calendar-link day-28'>March 8<br />John Patmos</a>
         <a onClick={this.props.clickLink} data-value = '2020-03-15' href= '#introPerson' className = 'calendar-link day-29'>March 15<br />Timothy</a>
         <a onClick={this.props.clickLink} data-value = '2020-03-22' href= '#introPerson' className = 'calendar-link day-30'>March 22<br />Lydia</a>
-        <a onClick={this.props.clickLink} data-value = '2020-03-29' href= '#introPerson' className = 'calendar-link day-31' style = {{fontSize: '16pt'}}>March 29<br />Prisca & Aquila </a>
+        <a onClick={this.props.clickLink} data-value = '2020-03-29' href= '#introPerson' className = 'calendar-link day-31' style = {{fontSize: '15pt'}}>March 29<br />Prisca & Aquila </a>
         <a onClick={this.props.clickLink} data-value = '2020-04-05' href= '#introPerson' className = 'calendar-link day-32'>April 5<br />Peter</a>
         <a onClick={this.props.clickLink} data-value = '2020-04-12' href= '#introPerson' className = 'calendar-link day-33'>April 12<br />Jesus</a>
         <a onClick={this.props.clickLink} data-value = '2020-04-19' href= '#introPerson' className = 'calendar-link day-34'>April 19<br />Paul</a>
-        <a onClick={this.props.clickLink} data-value = '2020-04-26' href= '#introPerson' className = 'calendar-link day-35'style = {{fontSize: '16pt'}}>April 26<br />Faithful Servant</a>
+        <a onClick={this.props.clickLink} data-value = '2020-04-26' href= '#introPerson' className = 'calendar-link day-35'style = {{fontSize: '14pt'}}>April 26<br />Faithful Servant</a>
         <a onClick={this.props.clickLink} data-value = '2020-05-03' href= '#introPerson' className = 'calendar-link day-36'>May 3<br />Sower</a>
         <a onClick={this.props.clickLink} data-value = '2020-05-10' href= '#introPerson' className = 'calendar-link day-37'>May 10<br />Good Samaritan</a>
         <a onClick={this.props.clickLink} data-value = '2020-05-17' href= '#introPerson' className = 'calendar-link day-38'>May 17<br />John</a>
